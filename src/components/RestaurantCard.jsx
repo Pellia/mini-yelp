cde41d64b8f6fc737a5317e6a4e5bded613232d1
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-03-01 12:25:54 +0100

    dummy data and card

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,7 +1,28 @@
-const RestaurantCard = () => {
+const RestaurantCard = ({ data }) => {
     return (
         <div className="flex justify-center">
-            <div className="bg-white p-6 w-96 h-32 rounded-2xl border border-neutral-300 mb-5">Restaurant Placeholder</div>
+            <div className="bg-white p-4 w-96 rounded-2xl border border-neutral-300 mb-5">
+                <p className="font-bold text-xl">{data.name}</p>
+                <div className="flex justify-between mt-15">
+                    <div className="flex gap-2">
+                        {data.city.map((c, i) => (
+                            <p key={i} className="p-1 bg-green-400 text-white rounded-md">
+                                {c}
+                            </p>
+                        ))}
+                    </div>
+                    <div className="flex gap-2">
+                        {data.tag.map((t, i) => (
+                            <p key={i} className="p-1 bg-blue-400 text-white rounded-md">
+                                {t}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex justify-end">
+                    <button className="p-1 text-blue-500 underline">See more..</button>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/data/dummy.js
@@ -0,0 +1,22 @@
+const dummyRestaurant = [
+    {
+        name: "rest1",
+        tag: ["chinese", "japanese"],
+        city: ["hamburg", "berlin"],
+        comments: ["i love it", "hate it"],
+    },
+    {
+        name: "rest2",
+        tag: ["german", "italian"],
+        city: ["frankfurt", "prag"],
+        comments: ["i love it", "hate it"],
+    },
+    {
+        name: "rest3",
+        tag: ["french"],
+        city: ["paris", "prag"],
+        comments: ["i love it", "hate it"],
+    },
+];
+
+export default dummyRestaurant;
