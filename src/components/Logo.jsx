d1007b5eb1f5b9ed8f232b35af9a50bef00947b7
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-03-01 10:39:07 +0100

    base restructe

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 const Logo = () => {
-    return <div className="flex justify-center font-bold text-4xl">Logo</div>;
+    return <div className="flex justify-center font-bold text-6xl my-8">Logo</div>;
 };
 
 export default Logo;
---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,9 @@
+const RestaurantCard = () => {
+    return (
+        <div className="flex justify-center">
+            <div className="bg-white p-6 w-96 h-32 rounded-2xl border border-neutral-300 mb-5">Restaurant Placeholder</div>
+        </div>
+    );
+};
+
+export default RestaurantCard;
