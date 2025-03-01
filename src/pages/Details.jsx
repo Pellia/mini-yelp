08152a17c59fe75788f1f61cc7445f3ec8852299
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-03-01 14:25:27 +0100

    finished base

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -10,7 +10,23 @@ const Details = () => {
         name: "rest1",
         tag: ["chinese", "japanese"],
         city: ["hamburg", "berlin"],
-        comments: ["i love it", "hate it"],
+        reviews: [
+            {
+                username: "user1",
+                comment: "i love it",
+                rating: "4/5",
+            },
+            {
+                username: "user2",
+                comment: "good",
+                rating: "5/5",
+            },
+            {
+                username: "user3",
+                comment: "nope",
+                rating: "3/5",
+            },
+        ],
     };
 
     useEffect(() => {
@@ -20,14 +36,53 @@ const Details = () => {
     }, [id]);
 
     return (
-        <div>
-            <h2>Details</h2>
-
-            {dummyRes && (
-                <div>
-                    <p>{dummyRes.name}</p>
-                </div>
-            )}
+        <div className="flex justify-center">
+            <div className="max-w-xl">
+                <hr className="border-2 border-neutral-300 mb-2 " />
+                {dummyRes && (
+                    <div className="flex flex-col items-center gap-4">
+                        <h2 className="text-4xl font-bold">{dummyRes.name}</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, dignissimos cum accusantium enim assumenda ab totam officiis, maiores similique, autem sunt nulla officia
+                            nostrum ea corrupti porro earum voluptatibus voluptatum! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod nostrum et sequi qui in eveniet dolorem sit eos
+                            ullam facilis dolores, vel libero dolorum fugit, consectetur molestias repellat magnam excepturi?
+                        </p>
+                        <div className="flex gap-6">
+                            <h3>Location</h3>
+                            <div className="flex gap-2">
+                                {dummyRes.city.map((c, i) => (
+                                    <p key={i} className="p-1 bg-green-400 text-white rounded-md">
+                                        {c}
+                                    </p>
+                                ))}
+                            </div>
+                            <h3>Tags</h3>
+                            <div className="flex gap-2">
+                                {dummyRes.tag.map((t, i) => (
+                                    <p key={i} className="p-1 bg-green-400 text-white rounded-md">
+                                        {t}
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+                        <h3>Map</h3>
+                        <div className="border border-neutral-300 rounded-xl bg-white w-96 h-96">Leaflet</div>
+                        <h3>Comments</h3>
+                        {dummyRes.reviews.map((user, i) => (
+                            <div key={i} className="bg-white border border-neutral-300 p-2 rounded-xl w-96">
+                                <p className="mb-3">User: {user.username}</p>
+                                <p>Comment</p>
+                                <div className="border-neutral-300 border p-4 rounded-2xl mb-3">
+                                    <p>{user.comment}</p>
+                                </div>
+                                <div className="flex justify-end">
+                                    <p>{user.rating} Stars</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,12 @@
 const Login = () => {
     return (
-        <div>
-            <div className="p-4">
+        <div className="flex justify-center">
+            <div className="bg-white rounded-xl p-4 border border-neutral-300 flex flex-col items-center gap-2 w-[35%]">
                 <h1 className="text-2xl font-bold">Login</h1>
-                <input type="text" placeholder="Username" className="border p-2 w-full mb-2" />
-                <input type="password" placeholder="Password" className="border p-2 w-full mb-2" />
-                <button className="bg-blue-500 text-white p-2 w-full">Login</button>
+                <input type="text" placeholder="Username" className="border border-neutral-300 rounded-lg w-96 h-12 p-2 bg-white text-lg" />
+                <input type="password" placeholder="Password" className="border border-neutral-300 rounded-lg w-96 h-12 p-2 bg-white text-lg" />
+                <button className="bg-pink-300 text-white font-bold p-2 w-96 rounded-xl">Login</button>
+                <p className="underline">Forgot Password</p>
             </div>
         </div>
     );
